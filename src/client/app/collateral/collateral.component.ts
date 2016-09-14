4d8752c0c9ccf3e9538feabb46f8c53c245bdb13
alefebvre-fixit <antoinelefebvre@gmail.com>
2016-09-13 21:34:08 -0700

    add accordion to menu

--- src/client/app/collateral/collateral.component.ts
+++ src/client/app/collateral/collateral.component.ts
@@ -3,6 +3,7 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 import {AlertComponent} from 'ng2-bootstrap/ng2-bootstrap';
 
 import { Config, ToolbarComponent } from '../shared/index';
+import {ACCORDION_DIRECTIVES, DROPDOWN_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
 /**
  * This class represents the main application component. Within the @Routes annotation is the configuration of the
@@ -13,7 +14,7 @@ import { Config, ToolbarComponent } from '../shared/index';
   viewProviders: [],
   templateUrl: 'collateral.component.html',
   styleUrls: ['collateral.component.css'],
-  directives: [ROUTER_DIRECTIVES, ToolbarComponent]
+  directives: [ROUTER_DIRECTIVES, ACCORDION_DIRECTIVES, ToolbarComponent]
 })
 export class CollateralComponent {
   constructor() {
